4c9dba9462a0d3efef7ce100b7b7bc94c5061060
digiovan <digiovan@cern.ch>
2015-04-04 23:22:54 +0200

    clean-up of files and added the switches for LT, LTB, BI as well

--- align_error/compute_align_errors.py
+++ align_error/compute_align_errors.py
@@ -9,10 +9,21 @@ from helpers import *
 
 def madx_format( a_string , ring):
 
-    a_string = a_string.replace ("PSB.BRMB",  "BR.BHZ").replace("0\"","$\"")
-    a_string = a_string.replace ("PSB.BRQF",  "BR.QF" ).replace("11\"","1$\"").replace("15\"","2$\"")
-    a_string = a_string.replace ("PSB.BRQD",  "BR.QD" ).replace("13\"","$\"")
-    a_string = a_string.replace ("PSB.BRUES", "BR"+ring+".BPM").replace("12\"","L3$\"")
+    if ('PSB.' in a_string):
+        a_string = a_string.replace ("PSB.BRMB",  "BR.BHZ").replace("0\"","$\"")
+        a_string = a_string.replace ("PSB.BRQF",  "BR.QF" ).replace("11\"","1$\"").replace("15\"","2$\"")
+        a_string = a_string.replace ("PSB.BRQD",  "BR.QD" ).replace("13\"","$\"")
+        a_string = a_string.replace ("PSB.BRUES", "BR"+ring+".BPM").replace("12\"","L3$\"")
+
+    if ('LT.'     in a_string or
+        'LTB.'    in a_string or    
+        'BI'      in a_string  ):
+        a_string = a_string.replace("\";","$\";")
+        
+    a_string = a_string.replace("BI.QNO50","BI"+ring+".QNO50")
+    a_string = a_string.replace("BI.QNO60","BI"+ring+".QNO60")
+    a_string = a_string.replace("BI.BPM40","BI"+ring+".BPM40")
+    a_string = a_string.replace("BI.BPM50","BI"+ring+".BPM50")
 
     return a_string
 
@@ -20,7 +31,28 @@ def append_in_array(ele,array):
     if (ele):
         array.append( float(ele) )
     else:
-        array.append ( -999999 )
+        array.append ( 0.0 )
+
+
+def ring_alignment(ele_name, dpsi, ring):
+    
+    if (ring == '3'):    
+        return 0.0
+
+    ele_to_correct = ['PSB','BI.QNO50','BI.QNO60','BI.BPM40','BI.QNO50']  
+    if not any(ele in ele_name for ele in ele_to_correct): 
+        return 0.0
+
+    if (ring == "4"):
+        return -0.360 * np.sin(dpsi)
+
+    if (ring == "2"):
+        return +0.360 * np.sin(dpsi)
+
+    if (ring == "1"):
+        return +0.720 * np.sin(dpsi)
+
+
 
 def printAlignErrs(lines,names,
                    offsets_hor,
@@ -127,12 +159,12 @@ def printAlignErrs(lines,names,
 
         #default variables    
         ele_name = "default name"
-        dx = -999
-        dy = -999
-        ds = -999
-        dphi   = -999
-        dtheta = -999
-        dpsi   = -999
+        dx = 0.0
+        dy = 0.0
+        ds = 0.0
+        dphi   = 0.0
+        dtheta = 0.0
+        dpsi   = 0.0
 
         index_E = names_E.index(baseName+".E")
         index_S = names_S.index(baseName+".S")
@@ -140,23 +172,33 @@ def printAlignErrs(lines,names,
         name_array = baseName.split(".")
         ele_name = name_array[0]+"."+name_array[1]+name_array[2]
         
-        dx =  offs_hor_E[ index_E ]
-        dy =  offs_ver_E[ index_E ]
-        ds = (offs_lon_E[ index_E ] + offs_lon_S[ index_S ])/2.
+        if (offs_hor_E[ index_E ]):
+            dx =  offs_hor_E[ index_E ]
+        if (offs_ver_E[ index_E ]):
+            dy =  offs_ver_E[ index_E ]
+        if (offs_lon_E[ index_E ] and offs_lon_S[ index_S ]):
+            ds = (offs_lon_E[ index_E ] + offs_lon_S[ index_S ])/2.
+            
+            ds_diff = lpos_S[index_S]-lpos_E[index_E]
+            if ds_diff:
+                dphi   = np.arctan( (offs_ver_S[index_S]-offs_ver_E[index_E]) / (lpos_S[index_S]-lpos_E[index_E]) )
+                dtheta = np.arctan( (offs_hor_S[index_S]-offs_hor_E[index_E]) / (lpos_S[index_S]-lpos_E[index_E]) )
 
-        dphi   = np.arctan( (offs_ver_S[index_S]-offs_ver_E[index_E]) / (lpos_S[index_S]-lpos_E[index_E]) )
-        dtheta = np.arctan( (offs_hor_S[index_S]-offs_hor_E[index_E]) / (lpos_S[index_S]-lpos_E[index_E]) )
-        dpsi   = -1.0 * tilts_E[ index_E ]
+        if (tilts_E[ index_E ]):
+            dpsi   = -1.0 * tilts_E[ index_E ]
 
-        if (ring == "4"):
-            dx -= 0.360 * np.sin(dpsi)
+        dx += ring_alignment(ele_name, dpsi, ring)
+    
+        #if (ring == "4"):
+        #    dx -= 0.360 * np.sin(dpsi)
+        #
+        #if (ring == "2"):
+        #    dx += 0.360 * np.sin(dpsi)
+        #
+        #if (ring == "1"):
+        #    dx += 0.720 * np.sin(dpsi)
 
-        if (ring == "2"):
-            dx += 0.360 * np.sin(dpsi)
-
-        if (ring == "1"):
-            dx += 0.720 * np.sin(dpsi)
-        
+        ########################################################################
         print "SELECT, FLAG = ERROR, CLEAR;"
         
         align_error  = "SELECT, FLAG = ERROR, PATTERN = "
@@ -179,6 +221,11 @@ import optparse
 parser = optparse.OptionParser()
 
 # define the options
+parser.add_option('-f', '--csvfile',
+                  help='specify the name of the csv file with the offsets',
+                  dest='CSVFILE',
+                  action='store')
+
 parser.add_option('-r', '--ring',
                   help='specify the ring',
                   dest='RING',
@@ -189,7 +236,7 @@ parser.add_option('-r', '--ring',
 
 
 # Sanity check: Making sure all mandatory options appeared
-mandatories = ['RING']
+mandatories = ['CSVFILE','RING']
 for m in mandatories:
     if not opts.__dict__[m]:
         print "at least the mandatory option %s is missing\n" % m
@@ -197,13 +244,20 @@ for m in mandatories:
         exit(-1)
 
 
-file_name = 'ecarts/PSB_Faisceau_Derniers_TheoriquesBumpees_Existant.csv'
+csvfilename = opts.CSVFILE
+
+if ('LT' in csvfilename and opts.RING != '3'):
+        print "\n\tLT and LTB lines are only at ring 3 height level, use \"-r 3\" option only\n"
+        exit(-1)
+
+#file_name = 'ecarts/PSB_OFFSETS_31MAR2015_Faisceau_Derniers_TheoriquesBumpees_Existant.csv'
+#file_name = 'ecarts/LT_OFFSETS_31MAR2015_Faisceau_Derniers_TheoriquesBumpees_Existant.csv'
 
 
 ################################################################################
 #
 columns = defaultdict(list)
-readCSV(file_name, columns)
+readCSV(csvfilename, columns)
 
 offsets_hor  = columns [4][1:]
 offsets_ver  = columns [7][1:]
@@ -212,18 +266,23 @@ tilts        = columns[10][1:]
 lonPos       = columns[17][1:]
 
 
-printAlignErrs ( columns [1][1:],
-                 columns [2][1:],
-                 offsets_hor,
-                 offsets_ver,
-                 offsets_lon,
-                 tilts,
-                 lonPos,
-                 opts.RING,
-                 #['BRMB'] )
-                 #['BRQF'] )
-                 #['BRQD'] )
-                 #['BRUES'] )
+ele_map = ['BRMB', 'BRQF', 'BRQD', 'BRUES']
 
-                 ['BRMB.110'] )
-                 #['QFO.311'] )
+if ('LT_'  in csvfilename or
+    'LTB_' in csvfilename or
+    'BI_'  in csvfilename  ):
+    ele_map = ['QFN','QDN','QFW','QDW','QNO']
+    
+
+for ele in ele_map:
+    printAlignErrs ( columns [1][1:],
+                     columns [2][1:],
+                     offsets_hor,
+                     offsets_ver,
+                     offsets_lon,
+                     tilts,
+                     lonPos,
+                     opts.RING,
+                     [ele] )
+    print '\n\n'
+
